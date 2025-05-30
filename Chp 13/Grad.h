38a34c60d96f46bdeb2e842283cca2305845429d
JONGHYUK02 <qkrwhdgur890@gmail.com>
2025-05-30 18:30:04 +0900

    20212047

---
+++ Chp 13/Grad.h
@@ -0,0 +1,24 @@
+#pragma
+// Grad.h
+
+#ifndef GUARD_Grad_h
+#define GUARD_Grad_h
+
+#include <iostream>
+
+#include "Core.h"
+
+using namespace std;
+
+class Grad : public Core {
+public:
+    Grad() : thesis(0) { };
+    Grad(istream& is) { read(is); };
+    double grade() const;
+    istream& read(istream&);
+
+private:
+    double thesis;
+    Grad* clone() const { return new Grad(*this); }
+};
+#endif
---
+++ Chp 13/median.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef GAURD_median_h
+#define GAURD_median_h
+
+// median.h
+#include "Vec.h"
+#include <vector>
+
+double median(vector<double>);
+
+
+#endif
